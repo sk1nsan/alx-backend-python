c1ae65aa4cadc5e41aecb014ff07a1a16177b71d
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-10-10 11:15:56 +0300

    task 12

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -5,4 +5,5 @@ from typing import Iterable, List, Tuple, Sequence
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """ type an interable object """
     return [(i, len(i)) for i in lst]
