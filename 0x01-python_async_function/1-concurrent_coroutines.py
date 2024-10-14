51c4344fad551fe5ed1883f206331ba2168e20b6
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-10-14 21:01:56 +0300

    task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -10,4 +10,4 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """ wait random time between 0 and max delay then return it """
     res = await asyncio.gather(*(wait_random(max_delay) for i in range(n)))
-    return res
+    return sorted(res)
