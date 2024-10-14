f186bb60f9bdd38549b378070f6571718f1ef4da
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-10-14 21:50:22 +0300

    task 4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -2,11 +2,12 @@
 """ Tasks """
 
 import asyncio
+from typing import List
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> asyncio.Task:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """ returns a asyncio.Task """
     r = await asyncio.gather(*(task_wait_random(max_delay) for i in range(n)))
     return r
