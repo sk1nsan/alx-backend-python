2e48b607a161abce1e3f64d555ae562fdcdd6cc7
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-10-14 21:34:31 +0300

    task 3

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -2,11 +2,10 @@
 """ tasks """
 
 import asyncio
-from typing import Any
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> asyncio.Task[Any]:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """ returns a asyncio.Task """
     return asyncio.create_task(wait_random(max_delay))
