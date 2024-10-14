b398e760a476072b17e847ea74bc14efc2b3c2e4
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-10-14 21:31:59 +0300

    fixed type

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -2,11 +2,11 @@
 """ tasks """
 
 import asyncio
-from typing import Coroutine, Any
+from typing import Any
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> Coroutine[Any, Any, float]:
+def task_wait_random(max_delay: int) -> asyncio.Task[Any]:
     """ returns a asyncio.Task """
     return asyncio.create_task(wait_random(max_delay))
