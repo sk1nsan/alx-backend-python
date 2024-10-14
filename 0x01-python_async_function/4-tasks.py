9a49f1b8812038754a1ebd64f4dabeee48131a6e
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-10-14 21:49:02 +0300

    task 4

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Tasks """
+
+import asyncio
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> asyncio.Task:
+    """ returns a asyncio.Task """
+    r = await asyncio.gather(*(task_wait_random(max_delay) for i in range(n)))
+    return r
